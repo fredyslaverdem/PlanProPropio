c54b4aef698b2da3ce2e741136255166df32fe78
nicolasmahecha1125 <nicolasmahecha1125@gmail.com>
2024-11-01 22:53:03 -0500

    nicolas-viewsagregarnotas

---
+++ app/src/main/java/com/utadeo/planpropropio/AgregarNotasActivity.kt
@@ -0,0 +1,14 @@
+package com.utadeo.planpropropio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AgregarNotasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_notas)
+        }
+    }
--- app/src/main/java/com/utadeo/planpropropio/HomeActivity.kt
+++ app/src/main/java/com/utadeo/planpropropio/HomeActivity.kt
@@ -7,18 +7,13 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // Difiniciendo variable para el layout principlal y asi el fondo se configure con ui del dispositivo.
-        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
 
-        // Obtener el color de fondo y aplicarlo a la barra de estado
-        val backgroundColor = (mainLayout.background as? ColorDrawable)?.color
-            ?: ContextCompat.getColor(this, R.color.colorFondoApp) // Color por defecto
 
-        window.statusBarColor = backgroundColor
 
     }
 }
