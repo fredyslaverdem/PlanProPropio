cec8f9b63b74ee0436b320ef1520ee3ab4f0adac
fredyslaverdem <fredys.laverdem@utadeo.edu.co>
2024-11-06 15:51:11 -0500

    Correciones layouts-land, vista de habitos

--- app/src/main/java/com/utadeo/planpropropio/DetalleHabitosActivity.kt
+++ app/src/main/java/com/utadeo/planpropropio/DetalleHabitosActivity.kt
@@ -1,14 +1,27 @@
 package com.utadeo.planpropropio
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DetalleHabitosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_consulta_actividad)
+
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+
+        // Obtener el color de fondo y aplicarlo a la barra de estado
+        val backgroundColor = (mainLayout.background as? ColorDrawable)?.color
+            ?: ContextCompat.getColor(this, R.color.colorFondoApp) // Color por defecto
+
+        window.statusBarColor = backgroundColor
+
     }
 }
